cd83dffb9e5c6d64a0ab283b92490a522b473160
Tim <tim@timsexperiments.foo>
2024-04-18 22:47:01 -0500

    Update turbo version

--- packages/plugin/eslint.config.mjs
+++ packages/plugin/eslint.config.mjs
@@ -1,4 +1,4 @@
-import baseConfig from 'eslint-config/ts-base.config.js';
+import baseConfig from '@timsexperiments/ts-plugin-workers-eslint/ts-base.config.js';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [...baseConfig];
